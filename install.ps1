18aca284b7e01485d7d13a7628d8784e453bc8dd
mattiewae <mathiaswaegemans@gmail.com>
2017-02-27 16:27:23 +0100

    default zip settings

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 ﻿Set-ExecutionPolicy RemoteSigned
 
 Set-Location -Path $home\Downloads
-wget https://github.com/mattiewae/test/raw/master/encoder.zip -OutFile .\encoder.zip
+wget https://github.com/mattiewae/EncoderInstall/blob/master/encoder.zip?raw=true -OutFile .\encoder.zip
 
-Expand-Archive -Path encoder.zip
+Expand-Archive -Path encoder.zip -DestinationPath test
 
