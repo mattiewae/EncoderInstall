377b6e8cbabe782d1a2fa7d9d1918178b7715ad6
mattiewae <mathiaswaegemans@gmail.com>
2017-02-27 16:08:35 +0100

    files

---
+++ install.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+Set-Location -Path $home\Downloads
+wget https://github.com/mattiewae/test/raw/master/encoder.zip -OutFile .\encoder.zip
+
+Expand-Archive -Path encoder.zip
+
