47058e8f53a73cabe478a0fb40689731662152b3
mattiewae <mathiaswaegemans@gmail.com>
2017-02-27 17:34:52 +0100

    update zip files

--- install.ps1
+++ install.ps1
@@ -2,7 +2,11 @@
 
 Set-Location -Path $home\Downloads
 wget https://github.com/mattiewae/EncoderInstall/blob/master/encoder.zip?raw=true -OutFile .\encoder.zip
+wget https://github.com/mattiewae/EncoderInstall/blob/master/exe.zip?raw=true -OutFile .\exe.zip
 
 Expand-Archive -Path .\encoder.zip
+Expand-Archive -Path .\exe.zip
 
-Move-Item .\encoder -Destination C:\ 
+Move-Item .\encoder -Destination C:\ -Force
+Move-Item .\exe\Encoder.lnk $home\Desktop -Force
+Move-Item .\exe\Encoder_V19.exe C:\encoder -Force
