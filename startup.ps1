541e6d5ce2ac715a4ce25ee1c4650c72a055e9d6
mattiewae <mathiaswaegemans@gmail.com>
2017-02-28 17:55:27 +0100

    test connection

--- startup.ps1
+++ startup.ps1
@@ -1,2 +1,10 @@
-choco install filezilla -y
-cup all -y 
+$Connect = Test-Connection 'www.google.com' -Quiet
+
+if($Connect = $true){
+    choco install filezilla -y
+    cup all -y 
+}
+else{
+    Start-Sleep -s 30
+    cup all -y
+}
