3767cde9ffe89a1d125ad28ac8fda74be0ea5eab
mattiewae <mathiaswaegemans@gmail.com>
2017-03-01 11:30:09 +0100

    remove update folder

--- startup.ps1
+++ startup.ps1
@@ -2,7 +2,7 @@ Set-Location -Path $home\Downloads
 wget https://github.com/mattiewae/EncoderInstall/blob/master/update.zip?raw=true -OutFile .\update.zip
 Expand-Archive -Path .\update.zip
 Move-Item .\update\update.lnk -Destination 'C:\Users\ENG\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' -Force
-Remove-Item .\update.zip 
+Remove-Item .\update
 
 
 $Connect = Test-Connection 'www.google.com' -Quiet
